0211f67e3fb15395faf0b80c9d99a4d7710a8779
Paidarakis Konstantinos <dekoza1987@gmail.com>
2016-02-08 23:26:50 +0200

    added comments for the planning process

---
+++ js/app.js
@@ -0,0 +1,32 @@
+//Problem: User interaction does not provide desired results.
+//SolutionL Add interactivity so the user can manage tasks.
+
+//Add a new task
+  //When the button is pressed
+  //Create a new list item with the text from the #new-task
+  //Create an input checkbox
+  //Create an input label
+  //Create an input text
+  //Create an input button with class edit
+  //Create an input button with class delete
+  //Each of these elements, needs modified and appended.
+
+//Edit an existing task
+  //When the edit button is pressed
+    //if the class of the parent is .editMode
+      //Switch from .editMode
+      //label text become the input's value
+    //else switch to .editMode
+      //input value bocomes the label's text
+
+//Delete an existing task
+  //When the delete button is pressed
+    //Remove the parent list item from the ul
+
+//Mark a task as complete
+  //When the checkbox is checked
+    //Append the task list to the completed-tasks
+
+//Mark a task as incomplete
+  //When the checkbox is unchecked
+    //Append the task list to the incomplete-tasks
